5310e93f5fbb09c4ca12a5e4491fbcc40387d339
Maurits Diephuis <mdiephuis@gmail.com>
2019-11-03 16:40:21 +0100

    skelton code setup with dcgan demo

---
+++ utils.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+
+# https://www.tensorflow.org/api_docs/python/tf/nn/sigmoid_cross_entropy_with_logits
+def loss_bce(x_hat, x):
+    loss = x_hat.clamp(min=0) - x_hat * x + torch.log(1 + torch.exp(-torch.abs(x_hat)))
+    return torch.mean(loss)
+
+
+def sample_uniform_noise(batch_size, dim):
+    return torch.Tensor(batch_size, dim).uniform_(-1, 1)
+
+
+def sample_gauss_noise(batch_size, dim):
+    return torch.Tensor(batch_size, dim).normal_(0, 1)
+
+
+def init_xavier_weights(module):
+    for m in module.modules():
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+            nn.init.xavier_uniform_(m.weight.data)
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias, 0.0)
+        elif isinstance(m, nn.Sequential):
+            for sub_mod in m:
+                init_xavier_weights(sub_mod)
+
+
+def dcgan_generation_example(G, noise_dim, n_samples, img_shape, use_cuda):
+
+    z_real = sample_uniform_noise(n_samples, noise_dim)
+    z_real = z_real.cuda() if use_cuda else z_real
+
+    x_hat = G(z_real).cpu().view(n_samples, img_shape[0], img_shape[1], img_shape[2])
+
+    # due to tanh output layer in the generator
+    x_hat = x_hat * 0.5 + 0.5
+
+    return x_hat
