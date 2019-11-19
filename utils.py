73b1c118806c3f2896037c4825d152b5fdfb42a4
diephuis <Maurits.Diephuis@unige.ch>
2019-11-19 17:14:41 +0100

    added bce, kld loses

--- utils.py
+++ utils.py
@@ -8,6 +8,11 @@ def loss_bce(x_hat, x):
     return torch.mean(loss)
 
 
+def loss_kl_gauss(mu, log_var):
+    KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+    return KLD
+
+
 def sample_uniform_noise(batch_size, dim):
     return torch.Tensor(batch_size, dim).uniform_(-1, 1)
 
@@ -36,5 +41,15 @@ def dcgan_generation_example(G, noise_dim, n_samples, img_shape, use_cuda):
 
     # due to tanh output layer in the generator
     x_hat = x_hat * 0.5 + 0.5
+
+    return x_hat
+
+
+def mnist_generation_example(G, noise_dim, n_samples, img_shape, use_cuda):
+
+    z_real = sample_uniform_noise(n_samples, noise_dim)
+    z_real = z_real.cuda() if use_cuda else z_real
+
+    x_hat = G(z_real).cpu().view(n_samples, img_shape[0], img_shape[1], img_shape[2])
 
     return x_hat
