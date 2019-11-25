ef07c0645af86d1822d343c03dcbe71cd07ce2dd
shidehrzf <shideh.rezaeifar@unige.ch>
2019-11-25 12:51:48 +0100

    fixed the generation bug

--- utils.py
+++ utils.py
@@ -32,6 +32,15 @@ def init_xavier_weights(module):
                 init_xavier_weights(sub_mod)
 
 
+def normal_weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
 def dcgan_generation_example(G, noise_dim, n_samples, img_shape, use_cuda):
 
     z_real = sample_uniform_noise(n_samples, noise_dim)
@@ -40,16 +49,38 @@ def dcgan_generation_example(G, noise_dim, n_samples, img_shape, use_cuda):
     x_hat = G(z_real).cpu().view(n_samples, img_shape[0], img_shape[1], img_shape[2])
 
     # due to tanh output layer in the generator
-    x_hat = x_hat * 0.5 + 0.5
+    #x_hat = x_hat * 0.5 + 0.5
 
     return x_hat
 
 
 def mnist_generation_example(G, noise_dim, n_samples, img_shape, use_cuda):
 
-    z_real = sample_uniform_noise(n_samples, noise_dim)
+    z_real = sample_gauss_noise(n_samples, noise_dim)
     z_real = z_real.cuda() if use_cuda else z_real
 
-    x_hat = G(z_real).cpu().view(n_samples, img_shape[0], img_shape[1], img_shape[2])
+    x_hat = G.decoder(z_real).cpu().view(n_samples, img_shape[0], img_shape[1], img_shape[2])
 
     return x_hat
+
+
+def dcgan_reconstruction_example(E, G, test_loader, n_samples, img_shape, use_cuda):
+    E.eval()
+    G.eval()
+
+    x, _ = next(iter(test_loader))
+    #x = x * 0.5 + 0.5
+    x = x.cuda() if use_cuda else x
+
+    x = x.view(-1, img_shape[0], img_shape[1], img_shape[2])
+
+    z_val, _, _ = E(x)
+
+    x_hat = G(z_val)
+    #x_hat = x_hat * 0.5 + 0.5
+
+    x = x[:n_samples].cpu().view(10 * img_shape[1], img_shape[2])
+    x_hat = x_hat[:n_samples].cpu().view(10 * img_shape[1], img_shape[2])
+    comparison = torch.cat((x, x_hat), 1).view(10 * img_shape[1], 2 * img_shape[2])
+
+    return comparison
